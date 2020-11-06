81056c3e6cc7ced6160d4316fbe037e7a0a33fc9
Albert-Wu-03 <22qwu@athenian.org>
2020-11-06 04:36:35 +0000

    Arraylist

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,20 +4,40 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int l = list.size();
+        int count = 0;
+        for(int i = 0; i < l; i++){
+            int n = list.get(i);
+            if(n % 2 != 0){
+                count++;
+            } 
+        }
+        return count;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
+        int length1 = list1.size();
+        int length2 = list2.size();
+        for(int i = 0; i < length1; i++){            
+            for(int j = 0; j < length2; j++){
+                int x = list1.get(i);
+                int y = list2.get(j);
+                if(x == y){
+                    return true;
+                } 
+            }
+        }
         return false;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < arr.length; i++){
+            list.add(arr[i]);
+        }
+        return list;
     }
 
 
